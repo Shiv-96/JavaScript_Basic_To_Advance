0a9e364e10e0b8f77d99c46156d635df3b568473
Shiv Kumar <101392263+Shiv-96@users.noreply.github.com>
2024-07-29 08:33:28 +0000

    02_variable

---
+++ JavaScript/02_variable.js
@@ -0,0 +1,13 @@
+const accountId = 1234567;
+let name = "Shiv Kumar";
+var city = "Patna";
+state = "Bihar";
+let country;
+
+// By using const keyword we can't change that value
+
+// It is preferred to use let instead of var
+
+console.log(accountId, name, city, state);
+
+console.table([accountId, name, city, state, country]);
